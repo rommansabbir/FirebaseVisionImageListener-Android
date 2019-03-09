f227aebf233a3d37af471c3771c0b2e0fca73f72
rommansabbir <misterxero96@gmail.com>
2019-03-09 22:34:54 +0600

    Removed Firebase dependency from App level, Package refactored

--- firebasevisionimagelistener/src/main/java/com/rommansabbir/firebasevisionimagelistener/FirebaseVisionImageListener.kt
+++ firebasevisionimagelistener/src/main/java/com/rommansabbir/firebasevisionimagelistener/FirebaseVisionImageListener.kt
@@ -1,35 +1,34 @@
 package com.rommansabbir.firebasevisionimagelistener
 
 import android.content.Context
-import android.util.Log
+import android.graphics.Bitmap
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.google.firebase.ml.vision.label.FirebaseVisionLabel
-import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetectorOptions
-import java.util.*
 import kotlin.collections.ArrayList
 
 class FirebaseVisionImageListener(context: Context) {
     private val TAG = "ImageListener"
     private var mContext : Context = context
     private var firebaseVisionImageListenerInterface: FirebaseVisionImageListenerInterface
-    private lateinit var labelsList : ArrayList<FirebaseVisionLabel>
+    private lateinit var labelsList : ArrayList<String>
 
     init {
         firebaseVisionImageListenerInterface = mContext as FirebaseVisionImageListenerInterface
     }
 
-    fun labelImages(image: FirebaseVisionImage) {
-        labelsList = ArrayList<FirebaseVisionLabel>()
+    fun getLabelsFromImage(bitmap: Bitmap) {
+        val image = FirebaseVisionImage.fromBitmap(bitmap)
+        labelsList = ArrayList<String>()
         val detector = FirebaseVision.getInstance().getVisionLabelDetector()
 
         detector.detectInImage(image).addOnSuccessListener(
                 object : OnSuccessListener<List<FirebaseVisionLabel>> {
                     override fun onSuccess(labels: List<FirebaseVisionLabel>) {
                         for (label in labels) {
-                            labelsList.add(label)
+                            labelsList.add(label.label.toString())
                         }
                         firebaseVisionImageListenerInterface.onFirebaseVisionImageSuccess(labelsList)
                     }
@@ -43,7 +42,7 @@ class FirebaseVisionImageListener(context: Context) {
     }
 
     interface FirebaseVisionImageListenerInterface{
-        fun onFirebaseVisionImageSuccess(firebaseVisionLabel: List<FirebaseVisionLabel>)
+        fun onFirebaseVisionImageSuccess(labelsList: List<String>)
         fun onFirebaseVisionImageFailure(exception : java.lang.Exception)
     }
 }
